4e7e3dd7bafa33fcc5d9b851a046c386cc613b27
gness1804 <graham.nessler@gmail.com>
2017-09-08 16:30:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/grahamnessler/athletesdatabase/Athlete.java
@@ -0,0 +1,46 @@
+package com.example.grahamnessler.athletesdatabase;
+
+/**
+ * Created by grahamnessler on 9/8/17.
+ */
+
+public class Athlete {
+    private String firstName;
+    private String lastName;
+    private String sport;
+    private double yearsExperience;
+    private int championships;
+
+    public String getFirstName () {
+        return "First name:" + this.firstName;
+    }
+
+    public String getLastName () {
+        return "Last name:" + this.lastName;
+    }
+
+    public String getSport () {
+        return "role:" + this.sport;
+    }
+
+    public String getExp () {
+        return "Experience: " + Double.toString(this.yearsExperience) + " years.";
+    }
+
+    public String getChampionships() {
+        return this.firstName + " has won " + this.championships + " championships.";
+    }
+
+    public void setFirstName (String name) {
+        this.firstName = name;
+    }
+
+
+    public Athlete (String first, String last, String sport, double exp, int championships) {
+        this.firstName = first;
+        this.lastName = last;
+        this.sport = sport;
+        this.yearsExperience = exp;
+        this.championships = championships;
+    }
+}
---
+++ app/src/main/java/com/example/grahamnessler/athletesdatabase/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.grahamnessler.athletesdatabase;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Athlete[] athletes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        instantiateLebron();
+
+    }
+
+    public void instantiateLebron () {
+        Athlete lbj = new Athlete("Lebron", "James", "Basketball", 13, 3);
+        TextView firstName = (TextView) findViewById(R.id.firstName);
+        firstName.setText(lbj.getFirstName());
+    }
+}
